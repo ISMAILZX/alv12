71036398ade0e283035ee227d2991a1ad034cc48
Gmail <77217686+6mail@users.noreply.github.com>
2021-12-10 21:53:34 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,5 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
-
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
-*💰 E-MONEY*
-
-1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
-	
-Semuanya atas nama RI****DI
+return `https://youtube.com/c/IsmailKurama
 `
 	}
 
