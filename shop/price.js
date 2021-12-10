394fe571710ddcf06037c4c3251e88fa31dbfb12
Gmail <77217686+6mail@users.noreply.github.com>
2021-12-10 21:52:05 +0700

    Update price.js

--- shop/price.js
+++ shop/price.js
@@ -1,19 +1,5 @@
 const list_price = (prefix) => {
-return `🎮 LIST GAME :
-
-${prefix}Pubg -> List Uc Pubg
-${prefix}Ml -> List Diamond MLBB
-${prefix}Ff -> List  Diamond FF
-${prefix}Cod -> List Cp Codm
-${prefix}Aov -> List Voucher Aov
-${prefix}Sausage -> List Candy Sausage
-${prefix}Lol -> List Wc Lol
-${prefix}Valo -> List Vp Valorant
-
-Untuk melihat listnya ketik ${prefix}list game 
-Ex :
-•> ${prefix}aov
-	ㅁ Untuk melihat list Voucher Aov
+return `https://youtube.com/c/IsmailKurama
 `
 	}
 
